2b68db01fdca961a048c9410f5199e60b6c1389b
Yvette <libra1014tyb@163.com>
2016-05-16 16:52:04 +0800

    complete

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,16 @@
-
 function collect_same_elements(collection_a, object_b) {
- //在此处写代码
+	var arr = object_b.value,
+		flag = 0,
+		collection = new Array();
+	for (var i = 0, length = collection_a.length; i < length; i++) {
+		for (var j = 0, len = arr.length; j < len; j++) {
+			if (collection_a[i].key == arr[j]) {
+				collection[flag] = collection_a[i].key;
+				flag++;
+			}
+		}
+	}
+	return collection;
 }
 
 module.exports = collect_same_elements;
